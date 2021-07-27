ec76d0915cecbb7a5604780e0dd26cff42b5722e
hzrasheed <alrasheedhana@gmail.com>
2021-07-27 09:53:29 +0300

    modification - question 2 is added

--- project/src/lab1/PracticeOne.java
+++ project/src/lab1/PracticeOne.java
@@ -16,6 +16,11 @@ public class PracticeOne {
 		}
 		
 	}
-	// 
+	// 2- 12 months to devide the year months into quartars 
+	// 12 months/ 4 = 3 months per quarter 
+	// 1 2 3 .> quarter 1
+	// 4 5 6 .> quarter 2
+	// 7 8 9 .> quarter 3
+	// 10 11 12 .> quarter 4  
 
 }
